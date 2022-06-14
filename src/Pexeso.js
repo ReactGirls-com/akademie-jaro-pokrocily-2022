b40aad73f60eee45aad64ee92484b045e4a694fd
Zuzka <zuzanastavjanova@gmail.com>
2022-06-14 06:07:05 +0200

    first lesson

---
+++ src/Pexeso.js
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+
+export default function Pexeso() {
+    return (
+        <div>
+            Hello React Girls from Pexeso
+            <Card image="/dogs/retriever.jpg" name="Brian" />
+            <Card image="/dogs/pug.jpg" name="Max" />
+            <Card image="/dogs/brooke.jpg" name="Brooke" />
+        </div>
+    );
+}
