3c78ff3379b0317b7d858213426a58ea9450d2be
Zuzka <zuzanastavjanova@gmail.com>
2022-06-20 21:08:06 +0200

    3lesson

--- src/Pexeso.js
+++ src/Pexeso.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import Board from "./components/Board";
 
+const dogs = [
+    "ret.jpg",
+    "pug.jpg",
+    "brooke.jpg",
+    "french.jpg",
+    "staff.jpeg",
+    "husky.jpg",
+];
+
 export default function Pexeso() {
+    const [difficulty, setDifficulty] = useState(null);
+
     return (
-        <div>
-            <Board />
+        <div className="bg-slate-50 h-screen flex flex-col gap-4 items-center justify-center">
+            {difficulty === null ? (
+                <>
+                    <Button title="Beginner" onClick={() => setDifficulty(1)} />
+                    <Button title="Normal" onClick={() => setDifficulty(2)} />
+                    <Button title="Expert" onClick={() => setDifficulty(3)} />
+                </>
+            ) : (
+                <Board
+                    key={difficulty}
+                    difficulty={difficulty}
+                    images={dogs.slice(0, difficulty + 1)}
+                    onGameFinished={(previousDifficulty) =>
+                        setDifficulty(previousDifficulty + 1)
+                    }
+                />
+            )}
         </div>
+    );
+}
+
+function Button(props) {
+    return (
+        <button
+            className="border p-2 px-4 rounded-md bg-slate-300"
+            onClick={props.onClick}
+        >
+            {props.title}
+        </button>
     );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,37 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
+import { shuffle } from "../utilities/shuffle";
 
-export default function Board() {
+export default function Board(props) {
     const [flippedCards, setFlippedCards] = useState([]);
+    const [foundCards, setFoundCards] = useState([]);
+    const [dogsDoubled] = useState(shuffle([...props.images, ...props.images]));
+
+    useEffect(() => {
+        if (flippedCards.length === 2) {
+            const flippedImages = flippedCards.map((idx) => {
+                return dogsDoubled[idx];
+            });
+            const doTheyMatch = flippedImages[0] === flippedImages[1];
+            if (doTheyMatch) {
+                setFoundCards([...foundCards, ...flippedCards]);
+                setFlippedCards([]);
+            } else {
+                setTimeout(() => {
+                    setFlippedCards([]);
+                }, 3000);
+            }
+        }
+    });
+
+    useEffect(() => {
+        if (foundCards.length === dogsDoubled.length) {
+            setTimeout(() => {
+                props.onGameFinished(props.difficulty);
+            }, 3000);
+        }
+    });
 
     function flipCard(index) {
-        if (flippedCards.length < 2) {
+        if (flippedCards.length < 2 && !flippedCards.includes(index)) {
             setFlippedCards([...flippedCards, index]);
         }
     }
 
     return (
         <div className="grid grid-cols-2 gap-4 p-4">
-            <Card
-                image="/dogs/ret.jpg"
-                flipped={flippedCards.includes(0)}
-                click={() => flipCard(0)}
-            />
-            <Card
-                image="/dogs/pug.jpg"
-                flipped={flippedCards.includes(1)}
-                click={() => flipCard(1)}
-            />
-            <Card
-                image="/dogs/ret.jpg"
-                flipped={flippedCards.includes(2)}
-                click={() => flipCard(2)}
-            />
-            <Card
-                image="/dogs/pug.jpg"
-                flipped={flippedCards.includes(3)}
-                click={() => flipCard(3)}
-            />
+            {dogsDoubled.map((src, idx) => (
+                <Card
+                    key={idx}
+                    image={"/dogs/" + src}
+                    flipped={
+                        flippedCards.includes(idx) || foundCards.includes(idx)
+                    }
+                    click={() => {
+                        flipCard(idx);
+                    }}
+                />
+            ))}
         </div>
     );
 }
