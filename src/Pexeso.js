796df2570ff4930ea19f0d8631bc6f7e1261d4b1
Zuzka <zuzanastavjanova@gmail.com>
2022-06-22 20:47:12 +0200

    Difficulty

--- src/Pexeso.js
+++ src/Pexeso.js
@@ -1,47 +1,36 @@
 import { useState } from "react";
 import Board from "./components/Board";
 
-const dogs = [
-    "ret.jpg",
-    "pug.jpg",
-    "brooke.jpg",
-    "french.jpg",
-    "staff.jpeg",
-    "husky.jpg",
-];
-
 export default function Pexeso() {
-    const [difficulty, setDifficulty] = useState(null);
-
+    const [difficulty, setDifficulty] = useState(2);
     return (
-        <div className="bg-slate-50 h-screen flex flex-col gap-4 items-center justify-center">
-            {difficulty === null ? (
-                <>
-                    <Button title="Beginner" onClick={() => setDifficulty(1)} />
-                    <Button title="Normal" onClick={() => setDifficulty(2)} />
-                    <Button title="Expert" onClick={() => setDifficulty(3)} />
-                </>
-            ) : (
-                <Board
-                    key={difficulty}
-                    difficulty={difficulty}
-                    images={dogs.slice(0, difficulty + 1)}
-                    onGameFinished={(previousDifficulty) =>
-                        setDifficulty(previousDifficulty + 1)
-                    }
+        <>
+            <div className="flex items-center justify-center gap-2 p-4">
+                <Button
+                    title="-"
+                    onClick={() => setDifficulty(difficulty - 2)}
                 />
-            )}
-        </div>
+                <input
+                    type="number"
+                    value={difficulty}
+                    onChange={(e) => setDifficulty(e.target.value)}
+                />
+                <Button
+                    title="+"
+                    onClick={() => setDifficulty(difficulty + 2)}
+                />
+            </div>
+            <div>
+                <Board count={difficulty} />
+            </div>
+        </>
     );
 }
 
-function Button(props) {
+function Button({ onClick, title }) {
     return (
-        <button
-            className="border p-2 px-4 rounded-md bg-slate-300"
-            onClick={props.onClick}
-        >
-            {props.title}
+        <button className="p-2 bg-pink-400 rounded-md" onClick={onClick}>
+            {title}
         </button>
     );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,35 +2,34 @@ import { useState, useEffect } from "react";
 import Card from "./Card";
 import { shuffle } from "../utilities/shuffle";
 
+function useDogsFromAPI(count) {
+    const [dogsDoubled, setDogsDoubled] = useState([]);
+
+    const refetch = () =>
+        fetch("https://dog.ceo/api/breeds/image/random/" + count)
+            .then((res) => res.json())
+            .then((json) => {
+                console.log("DATA JSOU ZDE", json);
+                const dogs = json.message;
+                const dogsDoubled = [...dogs, ...dogs];
+                const dogsDoubledAndShuffled = shuffle(dogsDoubled);
+                setDogsDoubled(dogsDoubledAndShuffled);
+            });
+
+    useEffect(() => {
+        refetch();
+    }, [count]);
+
+    return {
+        dogs: dogsDoubled,
+        refetch,
+    };
+}
+
 export default function Board(props) {
     const [flippedCards, setFlippedCards] = useState([]);
     const [foundCards, setFoundCards] = useState([]);
-    const [dogsDoubled] = useState(shuffle([...props.images, ...props.images]));
-
-    useEffect(() => {
-        if (flippedCards.length === 2) {
-            const flippedImages = flippedCards.map((idx) => {
-                return dogsDoubled[idx];
-            });
-            const doTheyMatch = flippedImages[0] === flippedImages[1];
-            if (doTheyMatch) {
-                setFoundCards([...foundCards, ...flippedCards]);
-                setFlippedCards([]);
-            } else {
-                setTimeout(() => {
-                    setFlippedCards([]);
-                }, 3000);
-            }
-        }
-    });
-
-    useEffect(() => {
-        if (foundCards.length === dogsDoubled.length) {
-            setTimeout(() => {
-                props.onGameFinished(props.difficulty);
-            }, 3000);
-        }
-    });
+    const { dogs, refetch } = useDogsFromAPI(props.count);
 
     function flipCard(index) {
         if (flippedCards.length < 2 && !flippedCards.includes(index)) {
@@ -38,20 +37,73 @@ export default function Board(props) {
         }
     }
 
+    function resetGame() {
+        setFlippedCards([]);
+        setFoundCards([]);
+        refetch();
+    }
+
+    useEffect(() => {
+        resetGame();
+    }, [props.count]);
+
+    useEffect(() => {
+        console.log("Effect happening", flippedCards);
+        if (flippedCards.length === 2) {
+            const flippedImages = flippedCards.map((idx) => {
+                return dogs[idx];
+            });
+            const doTheyMatch = flippedImages[0] === flippedImages[1];
+            if (doTheyMatch) {
+                setFoundCards([...foundCards, ...flippedCards]);
+                setFlippedCards([]);
+            } else {
+                const timeout = setTimeout(() => {
+                    setFlippedCards([]);
+                }, 2000);
+            }
+        }
+    });
+
+    const noOfColumns = Math.floor(Math.sqrt(props.count * 2));
+
+    if (foundCards.length === props.count * 2) {
+        return (
+            <div className="flex flex-col gap-4 justify-center items-center h-screen">
+                <h1 className="text-lg">Vyhrál jsi</h1>
+                <button
+                    className="bg-pink-500 text-white p-2 px-4 rounded-lg"
+                    onClick={() => resetGame()}
+                >
+                    Nová hra
+                </button>
+            </div>
+        );
+    }
+
     return (
-        <div className="grid grid-cols-2 gap-4 p-4">
-            {dogsDoubled.map((src, idx) => (
-                <Card
-                    key={idx}
-                    image={"/dogs/" + src}
-                    flipped={
-                        flippedCards.includes(idx) || foundCards.includes(idx)
-                    }
-                    click={() => {
-                        flipCard(idx);
-                    }}
-                />
-            ))}
-        </div>
+        <>
+            <div
+                className="grid gap-2 p-4"
+                style={{
+                    gridTemplateColumns:
+                        "repeat(" + noOfColumns + ", minmax(0, 1fr))",
+                }}
+            >
+                {dogs.map((src, idx) => (
+                    <Card
+                        key={idx}
+                        image={src}
+                        flipped={
+                            flippedCards.includes(idx) ||
+                            foundCards.includes(idx)
+                        }
+                        click={() => {
+                            flipCard(idx);
+                        }}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
